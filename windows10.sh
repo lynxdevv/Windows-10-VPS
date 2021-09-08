6d0e56f9b8139867e661eac05f15d00bf129593e
Karthi <86093775+ghost0506e@users.noreply.github.com>
2021-09-08 15:22:16 +0000

    Add files via upload

---
+++ windows10.sh
@@ -0,0 +1,20 @@
+echo "Download windows files"
+wget -O w10x64.vhdx https://bit.ly/2Ys4Uh5
+echo "Download ngrok"
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+read -p "Ctrl + V Authtoken: " CRP 
+./ngrok authtoken $CRP 
+nohup ./ngrok tcp 3388 &>/dev/null &
+echo Downloading File From akuh.net
+apt-get install qemu
+echo "Wait"
+echo "Starting Windows"
+echo RDP Address:
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+echo "Ctrl+C To Copy"
+echo "Wait for 5-10 minute to finish boot"
+echo "Dont Close This Tab"
+echo "Subscribe KTechy Today!!!"
+qemu-system-x86_64 -hda w10x64.vhdx -m 8G -smp cores=4 -net user,hostfwd=tcp::3388-:3389 -net nic -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -vga vmware -nographic
+sleep 43200
